1f774b9d78f9c27f86652e8276b523d10fa71792
egeongul <egeongul3217@gmail.com>
2023-10-31 17:01:57 +0300

    dashboard
    dashboard nav view eklendi

---
+++ app/src/main/java/view/Dashboard.kt
@@ -0,0 +1,36 @@
+package view
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
+import com.example.cs308_00.R
+
+class Dashboard : AppCompatActivity() {
+
+    lateinit var toggle : ActionBarDrawerToggle
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+
+        val drawerLayout : DrawerLayout = findViewById(R.id.drawerLayout1)
+
+
+        toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item)) {
+            return true
+        }
+        return false
+    }
+}
