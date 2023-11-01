464dc72603ac42780530afbc5d63b4c61450de0a
Zeynep Kurtulus <zeynepkurtulus@sabanciuniv.edu>
2023-11-01 18:11:31 +0300

    Dashboard Integration with Friends Room

--- app/src/main/java/view/FriendsRoom.kt
+++ app/src/main/java/view/FriendsRoom.kt
@@ -1,12 +1,50 @@
 package view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
+import android.widget.ImageButton
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.drawerlayout.widget.DrawerLayout.DrawerListener
+import androidx.lifecycle.ViewModelProvider
 import com.example.cs308_00.R
+import com.example.cs308_00.databinding.ActivityFriendsRoomBinding
+import viewmodel.FriendsRoomViewModel
+
 
 class FriendsRoom : AppCompatActivity() {
+    private lateinit var binding : ActivityFriendsRoomBinding
+    private lateinit var viewModel : FriendsRoomViewModel
+    lateinit var toggle : ActionBarDrawerToggle
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_friends_room)
+        //viewModel = ViewModelProvider(this).get(FriendsRoomViewModel::class.java)
+        //viewModel.setContext(this)
+
+        //var navBarBtn = findViewById<ImageButton>(R.id.navBarBtn)
+        val drawerLayout : DrawerLayout = findViewById(R.id.drawerLayout1)
+
+
+        toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+
+
+
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item)) {
+            return true
+        }
+        return false
     }
 }
--- app/src/main/java/viewmodel/FriendsRoomViewModel.kt
+++ app/src/main/java/viewmodel/FriendsRoomViewModel.kt
@@ -3,14 +3,16 @@ package viewmodel
 import android.content.Context
 import androidx.lifecycle.ViewModel
 import network.RetrofitClient
-import view.LoginPage
+import view.FriendsRoom
 
 class FriendsRoomViewModel() : ViewModel() {
     private lateinit var ctx: Context
     private lateinit var retrofitClient: RetrofitClient
 
-    fun setContext(ctx: LoginPage) {
+    fun setContext(ctx: FriendsRoom) {
         this.ctx = ctx
         retrofitClient = RetrofitClient(ctx)
     }
+
+
 }
