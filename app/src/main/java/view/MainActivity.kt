3370042307d94e5ec67562a6a03e85968e994fdf
Zeynep Kurtulus <zeynepkurtulus@sabanciuniv.edu>
2023-11-02 15:40:15 +0300

    Right SideBar & RegisterPage UI implementation

--- app/src/main/java/view/MainActivity.kt
+++ app/src/main/java/view/MainActivity.kt
@@ -1,7 +1,9 @@
 package view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.view.Window
 import com.example.cs308_00.R
 
@@ -9,6 +11,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Handler().postDelayed({
+            startActivity(Intent(this, LoginPage::class.java))
+            finish()
+        },2000
+
+        )
 
     }
 }
