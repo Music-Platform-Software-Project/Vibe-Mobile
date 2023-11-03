e9267777fa95dcb6048de438183dd6b8bc0055d7
Zeynep Kurtulus <zeynepkurtulus@sabanciuniv.edu>
2023-11-04 00:07:26 +0300

    Profile Page UI implementation

--- app/src/main/java/view/ProfilePage.kt
+++ app/src/main/java/view/ProfilePage.kt
@@ -2,11 +2,41 @@ package view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
 import com.example.cs308_00.R
+import com.example.cs308_00.databinding.ActivityFriendsRoomBinding
+import com.example.cs308_00.databinding.ActivityProfilePageBinding
+import viewmodel.FriendsRoomViewModel
+import viewmodel.ProfilePageViewModel
 
 class ProfilePage : AppCompatActivity() {
+    private lateinit var binding : ActivityProfilePageBinding
+    private lateinit var viewModel : ProfilePageViewModel
+    lateinit var toggle : ActionBarDrawerToggle
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile_page)
+
+        //var navBarBtn = findViewById<ImageButton>(R.id.navBarBtn)
+        val drawerLayout : DrawerLayout = findViewById(R.id.drawerLayout1)
+
+
+        toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
     }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item)) {
+            return true
+        }
+        return false
+    }
+
+
+
 }
