3180f8a0ad6f13f1a5143a39103b5c4e0546f448
Zeynep Kurtulus <zeynepkurtulus@sabanciuniv.edu>
2024-01-18 23:54:16 +0300

    Implementation of Sound Functionality for Fire

--- app/src/main/java/view/my_room.kt
+++ app/src/main/java/view/my_room.kt
@@ -1,25 +1,36 @@
 package view
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.media.AudioManager
+import android.media.MediaPlayer
+import android.os.Build
 import android.os.Bundle
 import android.view.MenuItem
 import android.view.View
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.SeekBar
+import android.widget.SeekBar.OnSeekBarChangeListener
 import android.widget.TextView
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.lifecycle.ViewModelProvider
 import com.example.cs308_00.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.nitish.typewriterview.TypeWriterView
 import com.squareup.picasso.Picasso
 import viewmodel.DashboardViewModel
+
 
 class my_room : AppCompatActivity() {
 
     lateinit var toggle : ActionBarDrawerToggle
     private lateinit var viewModel: DashboardViewModel
+    private val maxProgress = 450 // you can set it as you want
+    private val minVolumeLevel = 0.1f // Adjust this value as needed (0.0 to 1.0)
+    private val maxVolumeLevel = 35.0f // Adjust this value as needed (0.0 to 1.0)
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,6 +48,42 @@ class my_room : AppCompatActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.setHomeButtonEnabled(true)
         supportActionBar?.setDisplayShowHomeEnabled(true)
+
+
+        val fireSeekBar : SeekBar = findViewById(R.id.slider_fireplace)
+        fireSeekBar.max = maxProgress
+        var mediaPlayer: MediaPlayer? = null
+
+        fireSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                // Calculate the volume level based on progress
+                val volumeLevel =
+                    minVolumeLevel + (1.0f - minVolumeLevel) * (progress.toFloat() / maxProgress)
+                mediaPlayer?.setVolume(volumeLevel, volumeLevel)
+                mediaPlayer?.setVolume(volumeLevel, volumeLevel)
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                // Nothing to do here
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                // Nothing to do here
+            }
+        })
+
+        val firePlay : FloatingActionButton = findViewById(R.id.firePlaceBtn)
+        firePlay.setOnClickListener {
+            mediaPlayer?.release()
+            mediaPlayer = MediaPlayer.create(this, R.raw.fireplace)
+
+            // Set the initial volume based on the SeekBar progress
+            val initialVolumeLevel = minVolumeLevel + (maxVolumeLevel - minVolumeLevel) * (fireSeekBar.progress.toFloat() / maxProgress)
+            mediaPlayer?.setVolume(initialVolumeLevel, initialVolumeLevel)
+
+
+            mediaPlayer?.start()
+        }
 
 
         /*
@@ -83,7 +130,6 @@ class my_room : AppCompatActivity() {
 
 
     }
-
 
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
